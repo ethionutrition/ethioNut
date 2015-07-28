63c9b01f2588d62538364cce03470e970db133a8
yordan <yordan.desta@gmail.com>
2015-07-28 21:25:29 +0300

    Formating of User and UserProfile Domain Models, Added and started tracking ReferenceData.sql and TestData.sql Scripts

--- src/EthioNutrition.Data/Models/User.cs
+++ src/EthioNutrition.Data/Models/User.cs
@@ -10,13 +10,21 @@ namespace EthioNutrition.Data.Models
     public class User:IVersionedModelObject
     {
         public virtual Guid UserId { get; set; }
+        
         public virtual string Email { get; set; }
+        
         public virtual string UserName { get; set; }
+        
         public virtual string FirstName { get; set; }
+        
         public virtual string LastName { get; set; }
+        
         public virtual DateTime DateCreated { get; set; }
+        
         public virtual byte[] Version{get;set;}
+        
         public virtual UserProfile profile { get; set; }       
+    
     }
 }
 
--- src/EthioNutrition.Data/Models/UserProfile.cs
+++ src/EthioNutrition.Data/Models/UserProfile.cs
@@ -10,11 +10,16 @@ namespace EthioNutrition.Data.Models
     public class UserProfile: IVersionedModelObject
     {
         public virtual long ProfileID { get; set; }
+        
         public virtual DateTime? UserBirthDate { get; set; }
+        
         public virtual long? UserHeightInMeter { get; set; }
+        
         public virtual long? UserWeightInKg { get; set; }
+        
         //user BMI should be set internally
         public virtual long? UserBMI { get;  set; }    
+        
         public byte[] Version { get; set; }
 
     }
